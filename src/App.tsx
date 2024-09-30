668cab31bb2fb192b0597679f4ef2a48fb95fa20
Davbkim0420@gmail.com <Davbkim0420@gmail.com>
2024-09-30 19:34:27 -0400

    MP-2 done, did some goofy stuff with transitions.

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import Exchange from "./components/Currency.tsx";
+import styled from "styled-components";
+import {useEffect, useState} from "react";
+import {Currency} from "./interfaces/Currencies.ts";
+
+const ParentDiv=styled.div`
+    width: 80vw;
+    margin: auto;
+    border: 5px black solid;
+`;
+
+export default function App(){
+
+    // useState Hook to store Data.
+    const [data, setData] = useState<Currency[]>([]);
+
+    // useEffect Hook for error handling and re-rendering.
+    useEffect(() => {
+        async function fetchData(): Promise<void> {
+            const rawData = await fetch("https://api.coinbase.com/v2/currencies");
+            const {data} : {data:Currency[]} = await rawData.json();
+            setData(data);
+        }
+        fetchData()
+            .then(() => console.log("Data fetched successfully"))
+            .catch((e: Error) => console.log("There was the error: " + e));
+    }, [data.length]);
+
+    return(
+        <ParentDiv>
+            <Exchange data={data}/>
+        </ParentDiv>
+    )
+}
---
+++ src/components/Currency.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import {Currency} from "../interfaces/Currencies.ts";
+
+const AllCharsDiv=styled.div`
+    display: flex;
+    flex-flow: row wrap;    
+    justify-content: space-evenly;
+    background-color: black;
+`;
+
+const SingleCharDiv=styled.div<{name: string}>`
+    display: flex;
+    flex-direction: column;   
+    justify-content: center;
+    max-width: 30%;
+    padding: 2%;
+    margin: 1%;
+    background-color: white;
+    color: black; 
+    border: 3px white;
+    font: italic small-caps bold calc(2px + 1vw) Impact, "Times New Roman";
+    text-align: center;
+    transition: transform .5s ease-in-out;
+    &:hover{transform:scale(2.0)};
+`;
+
+export default function Exchange(props : { data:Currency[] } ){
+    return (
+        <AllCharsDiv >
+            {
+                props.data.map((char: Currency) =>
+                    <SingleCharDiv key={char.id} name ={char.name}>
+                        <h1>{char.id}</h1>
+                        <p>{char.min_size}</p>
+                        <p>{char.name}</p>
+                    </SingleCharDiv>
+                )
+            }
+        </AllCharsDiv>
+    );
+}
